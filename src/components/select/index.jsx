6214f5ee03ae2cb229a7f536d72cdcda8b9b0855
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-28 10:35:36 -0300

    componente select
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: juliabb <juliabenedictohfvcjd@gmail.com>
    Co-authored-by: leticiaantunesjpeg <leticiaantunes95@outlook.com>
    Co-authored-by: suelenescorcio <suelen919@gmail.com>
    Co-authored-by: vanessavb92 <vanessacamahuali28@gmail.com>

---
+++ src/components/select/index.jsx
@@ -0,0 +1,18 @@
+// function Select({onChange,options}) {
+//   return (
+//     <>
+//     <select onChange={onChange}>
+//       <option>Selecione</option>
+//       {options.map((options, index) => {
+//       return(
+//         <option key={index} value={options.name}>
+//           {options.uf}
+//         </option>
+//       );
+//     })}
+//     </select>
+//     </>
+//   );
+// }
+
+// export default Select;
