d3726975acdce7f09617b5753ebea2734ec8b2a3
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-06-28 15:02:56 -0300

    buscando cursos pelo nome

--- src/pages/comparator/index.jsx
+++ src/pages/comparator/index.jsx
@@ -1,42 +1,46 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Input from '../../components/input';
+import Header from '../../components/header';
+// import CoursesCard from '../../components/coursesCard';
 // import Select from '../../components/select';
-import { getInstitutions} from '../../services/api';
-
+import { getInstitutions } from '../../services/api';
 
 function Comparator() {
   //const [dataStates, setDataStates] = useState([]);
   const [institutions, setInstitutions] = useState([]);
   const [text, setText] = useState('');
 
-  useEffect(() => {
-    if(text === '') {
-    getInstitutions()
-    .then((response) => response.json())
-    .then((data) => {
-      //setDataStates(data);
-      setInstitutions(data);
-      console.log(data, 'data');
-    });
-  } else if (text !== '' ){
-    getInstitutions()
-    .then((response) => response.json())
-    .then((data) => {
-      setInstitutions(data.filter(elem => elem.name === text));
-    });
+  function filterCourses(data, text) {
+    let courseFiltered =  data.filter((course) => course.name === text);
+    return courseFiltered;
+    // console.log(courseFiltered);
   }
-  }, []);
 
-  console.log(institutions, 'instituição');
-  //console.log(dataStates, 'states');
-  console.log(text);
+  useEffect(() => {
+    getInstitutions()
+      .then((response) => response.json())
+      .then((data) => {
+        setInstitutions(data.institutions);
+        filterCourses(institutions, text);
+      });
+  }, [text]);
+
+  console.log(institutions);
+  filterCourses(institutions, 'UNISUL');
+
   return (
     <>
-    <Input type='search' value={text} onChange={(e) => setText(e.target.value)}
-    placeholder='Pesquise o curso ou a instituição' className='search-input' />
+      <Header className="" />
+      <Input
+        type="search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Pesquise o curso ou a instituição"
+        className="search-input"
+      />
+      {/* <CoursesCard institutions={institutions} /> */}
     </>
   );
-
 }
 
 export default Comparator;
