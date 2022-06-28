36e8fe94da6c4921ea9bdd5a516f9ad14e67a596
suelenescorcio <suelen919@gmail.com>
2022-06-27 21:54:41 -0300

    correção eslint

--- src/pages/comparator/comparator.jsx
+++ src/pages/comparator/comparator.jsx
@@ -1,7 +1,7 @@
 function Comparator() {
     return(
         <h1>OI</h1>
-    )
+    );
 }
 
 export default Comparator;
--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/home';
+import Home from './pages/home/index';
 import Comparator from './pages/comparator/comparator';
 
 const AllRoutes = () => {
@@ -14,6 +14,6 @@ const AllRoutes = () => {
       </div>
     </Router>
   );
-}
-  
+};
+
 export default AllRoutes;
