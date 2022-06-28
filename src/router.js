862c7018121711c8e8fe9a02dd58b54c1d20737e
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-28 10:35:55 -0300

    rotas pagina comparador
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: juliabb <juliabenedictohfvcjd@gmail.com>
    Co-authored-by: leticiaantunesjpeg <leticiaantunes95@outlook.com>
    Co-authored-by: suelenescorcio <suelen919@gmail.com>
    Co-authored-by: vanessavb92 <vanessacamahuali28@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/index';
-import Comparator from './pages/comparator/comparator';
+import Comparator from './pages/comparator/index';
 
 const AllRoutes = () => {
   return (
