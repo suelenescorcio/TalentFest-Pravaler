f32ec50a60f9fc5151110fdc5e156e2d7d21f47c
Leticia Antunes Almeida <leticiaantunes@outlook.com>
2022-06-29 13:41:03 -0300

    página comparador estilizada

--- src/components/coursesCard/index.jsx
+++ src/components/coursesCard/index.jsx
@@ -3,22 +3,19 @@ import Button from '../button';
 
 function CoursesCard({courses, onclick}) {
   return (
-    // <div className='card-courses'>
       courses.map((course, key) => {
           return (
             <li className='card-courses' key={key}>
-              <p>Nome do Curso: {course.name}</p>
-              <p>Descrição: {course.description}</p>
-              <p>Semestres: {course.semesters}</p>
-              <p>Mensalidade: {course.monthly_payment}</p>
-              <p>Nota do MEC: {course.mec_avaliation}</p>
-              <p>Faixa Salarial: {course.salary_range}</p>
+              <p><span>Nome do Curso: </span>{course.name}</p>
+              <p><span>Descrição: </span>{course.description}</p>
+              <p><span>Semestres: </span>{course.semesters}</p>
+              <p><span>Mensalidade: </span>R$ {course.monthly_payment}</p>
+              <p><span>Nota do MEC: </span>{course.mec_avaliation}</p>
+              <p><span>Faixa Salarial: </span>R$ {course.salary_range}</p>
               <Button type='click' onClick={onclick} className='button-course'>Comparar</Button>
             </li>
           );
         })
-
-    // </div>
   );
 }
 
--- src/pages/comparator/index.jsx
+++ src/pages/comparator/index.jsx
@@ -2,9 +2,15 @@ import { useEffect, useState } from 'react';
 import Input from '../../components/input';
 import Header from '../../components/header';
 import CoursesCard from '../../components/coursesCard';
-import { getCourses, getInstitutions, getStates, getCampus } from '../../services/api';
+import {
+  getCourses,
+  getInstitutions,
+  getStates,
+  getCampus,
+} from '../../services/api';
 import Select from '../../components/select';
 import Table from '../../components/table';
+import Footer from '../../components/footer';
 import './style.css';
 
 function Comparator() {
@@ -16,7 +22,9 @@ function Comparator() {
   const [text, setText] = useState('');
 
   function filterCourses(data, text) {
-    let courseFiltered = data.filter((response) => response.name.includes(text));
+    let courseFiltered = data.filter((response) =>
+      response.name.includes(text)
+    );
     setCourses(courseFiltered);
     return courseFiltered;
     //console.log(courseFiltered);
@@ -24,31 +32,31 @@ function Comparator() {
 
   useEffect(() => {
     getInstitutions()
-    .then((response) => response.json())
-    .then((data) => {
-      const dataInstitution = data.institutions;
-      setInstitutions(dataInstitution);
-      // console.log(data, 'data');
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        const dataInstitution = data.institutions;
+        setInstitutions(dataInstitution);
+        // console.log(data, 'data');
+      });
   }, []);
 
-    useEffect(() => {
-      getCampus()
+  useEffect(() => {
+    getCampus()
       .then((response) => response.json())
       .then((data) => {
         const dataCampus = data.campus;
         setCampus(dataCampus);
         // console.log(data, 'data');
       });
-    }, []);
+  }, []);
   useEffect(() => {
     getStates()
-    .then((response) => response.json())
-    .then((data) => {
-      const states = data.states;
-      setDataStates(states);
-      console.log(data, 'data');
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        const states = data.states;
+        setDataStates(states);
+        console.log(data, 'data');
+      });
   }, []);
 
   useEffect(() => {
@@ -59,48 +67,47 @@ function Comparator() {
       });
   }, []);
 
- const handleInput = (e) => {
+  const handleInput = (e) => {
     setText(e.target.value);
     filterCourses(courses, text);
   };
 
-  const handleCourse = (course) =>{
+  const handleCourse = (course) => {
     const courseFind = courses.find((item) => item.id === course.id);
     // setCourseSelected([...course]);
-       if (courseFind) {
-        courseFind.name += 1;
+    if (courseFind) {
+      courseFind.name += 1;
     } else {
       course.name = 1;
       courses.push(course);
     }
     setCourseSelected([...courses]);
-
   };
-
-
-
-
 
   return (
     <>
-    <main>
-      <Header className="logo-comparator" />
-      <Input
-        type="search"
-        value={text}
-        onChange={handleInput}
-        placeholder="Pesquise o curso ou a instituição"
-        className="search-input"
+      <main>
+        <Header className="logo-comparator" />
+        <Input
+          type="search"
+          value={text}
+          onChange={handleInput}
+          placeholder="Pesquise o curso ou a instituição"
+          className="search-input"
         />
-      <Select options={institutions}/>
-      <Select options={campus} />
-      <Select options={dataStates} />
-      {courseSelected.map((course) => {
-        <Table course={course}/>;
-      })}
-        <section className='section-cards'>
-      <CoursesCard courses={courses} onclick={() => handleCourse(courses)}/>
-      </section>
+        <Select options={institutions} />
+        <Select options={campus} />
+        <Select options={dataStates} />
+        {courseSelected.map((course) => {
+          <Table course={course} />;
+        })}
+        <section className="section-cards">
+          <CoursesCard
+            courses={courses}
+            onclick={() => handleCourse(courses)}
+          />
+        </section>
+        <Footer />
       </main>
     </>
   );
