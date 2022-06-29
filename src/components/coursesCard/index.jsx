b4be4cc9d54c398460998278ef5988f67d12c5a4
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-29 19:40:20 -0300

    componente coursesCard botão selecionar curso

--- src/components/coursesCard/index.jsx
+++ src/components/coursesCard/index.jsx
@@ -1,18 +1,23 @@
 import './style.css';
 import Button from '../button';
 
-function CoursesCard({courses, onclick}) {
+function CoursesCard({courses, courseSelected, setCourseSelected}) {
+
+  const handleSelectedCourse = (course) => {
+    setCourseSelected([...courseSelected, course]);
+  };
+
   return (
-      courses.map((course, key) => {
+      courses.map((course) => {
           return (
-            <li className='card-courses' key={key}>
+            <li className='card-courses' key={course.id}>
               <p><span>Nome do Curso: </span>{course.name}</p>
               <p><span>Descrição: </span>{course.description}</p>
               <p><span>Semestres: </span>{course.semesters}</p>
               <p><span>Mensalidade: </span>R$ {course.monthly_payment}</p>
               <p><span>Nota do MEC: </span>{course.mec_avaliation}</p>
               <p><span>Faixa Salarial: </span>R$ {course.salary_range}</p>
-              <Button type='click' onClick={onclick} className='button-course'>Comparar</Button>
+              <Button type='click' onClick={() => handleSelectedCourse(course)} className='button-course'>Comparar</Button>
             </li>
           );
         })
