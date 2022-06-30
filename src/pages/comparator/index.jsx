196dd025c0a0b58701b8b42375f45d4930bf2029
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-30 12:29:39 -0300

    correção no filtro UF

--- src/pages/comparator/index.jsx
+++ src/pages/comparator/index.jsx
@@ -52,11 +52,13 @@ function Comparator() {
     await getCampus()
       .then((response) => response.json())
       .then((data) => {
-        const states = data.campus;
-        setDataStates(states);
+        const newStates = data.campus.map(state => state.uf);
+        const filteredStates = [... new Set(newStates)];
+        console.log(filteredStates);
+        setDataStates(filteredStates);
+
       });
   };
-
 
   const fetchCourses = async () => {
     await getCourses()
@@ -84,8 +86,8 @@ function Comparator() {
 
   const handleReset = () => {
     setText('');
-    setInstitutions([]);
-    setCampus([]);
+    setInstitutions();
+    setCampus();
     fetchInstitutions();
     fetchCampus();
     fetchCourses();
@@ -156,7 +158,7 @@ function Comparator() {
             onClick={handleComparator}
             className="button-reset-comparation"
           >
-            Refazer comporativo
+            Refazer comparativo
           </Button>
           </div>
         </div>
