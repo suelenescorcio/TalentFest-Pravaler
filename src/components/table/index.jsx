6c27be09bf192886120e6766d6870e33271881fa
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-30 09:35:22 -0300

    alteração layout tabela
    Co-authored-by: flavia-dantas <flavia.dantas2@hotmail.com>
    Co-authored-by: juliabb <juliabenedictohfvcjd@gmail.com>
    Co-authored-by: leticiaantunesjpeg <leticiaantunes95@outlook.com>
    Co-authored-by: suelenescorcio <suelen919@gmail.com>
    Co-authored-by: vanessavb92 <vanessacamahuali28@gmail.com>

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -3,20 +3,20 @@ import './style.css';
 
 function Table({ arrCourse }) {
     const navigate = useNavigate();
-    
+
     function nameCourse(courseSelectedee) {
       navigate('/dice');
       localStorage.setItem('Name', courseSelectedee.name);
       }
-  
+
   return (
     <>
       <table>
         <thead>
           <tr>
             <th className="th">Curso</th>
-            <th className="th">Instituição</th>
-            <th className="th">Campus</th>
+            {/* <th className="th">Instituição</th>
+            <th className="th">Campus</th> */}
             <th className="th">Mensalidade</th>
             <th className="th">Nota MEC</th>
             <th className="th">Qtd. Semestre</th>
@@ -29,8 +29,8 @@ function Table({ arrCourse }) {
             return (
               <tr key={course.id}>
                 <td>{course.name}</td>
-                <td>{course.instituionId}</td>
-                <td>{course.campusId}</td>
+                {/* <td>{course.instituionId}</td>
+                <td>{course.campusId}</td> */}
                 <td>R$ {course.monthly_payment}</td>
                 <td>{course.mec_avaliation}</td>
                 <td>{course.semesters}</td>
