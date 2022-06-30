be5f8045791ec6a1e0feb263edde51e6a9326b72
flavia-dantas <flavia.dantas2@hotmail.com>
2022-06-30 02:23:24 -0300

    Componente select uf

---
+++ src/components/selectUf/index.jsx
@@ -0,0 +1,24 @@
+import './style.css';
+
+function SelectUf({ onChange, options,children }) {
+  return (
+    <>
+      <select onChange={onChange} className='select-option'>
+      <option disabled selected>
+      {children}
+
+        </option>
+        {options &&
+         options.map((option, index) => {
+          return (
+            <option key={index} value={option.instituionId}>
+              {option.uf}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+}
+
+export default SelectUf;
